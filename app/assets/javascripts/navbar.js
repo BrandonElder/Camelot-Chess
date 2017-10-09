c084ca2633fcd1cccb21ed514c47a54b7c86615b
Brandon Elder <brandonelder3@gmail.com>
2017-10-09 04:18:09 +0000

    redesigned navbar and cleaned up static index markup

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,17 +1,24 @@
- $(document).ready(function() {
+
+document.addEventListener("turbolinks:load", function() {
   var menuToggle = $("#js-mobile-menu").unbind();
   $("#js-navigation-menu").removeClass("show");
 
   menuToggle.on("click", function(e) {
     e.preventDefault();
-    $('.navigation-wrapper').toggleClass('active');
-    $('.hero').toggleClass('active');
     $('.burger-toggle').toggleClass('opened');
+    if ($('.hero').hasClass('active')) {
+      $('.hero').removeClass('active');  
+    } else {
+      $('.hero').addClass('active');
+      $('.hero').removeClass('active');
+    }
+    if ($('.navigation-wrapper').hasClass('clear')) {
+      $('.navigation-wrapper').removeClass('clear');
+    }
     $("#js-navigation-menu").slideToggle(function(){
-      if($("#js-navigation-menu").is(":hidden")) {
+      if ($("#js-navigation-menu").is(":hidden")) {
         $("#js-navigation-menu").removeAttr("style");
-      }
+      } 
     });
-  });
+  }); 
 }); 
-
