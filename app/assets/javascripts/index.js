494820a2f061248e55e3b0c820f1d8814204c01b
Brandon Elder <brandonelder3@gmail.com>
2018-05-06 14:40:52 +0000

    changed landing page to grid layout

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,8 +2,7 @@ document.addEventListener("turbolinks:load", function() {
   $(function() {
     $('#arrows').click(function() {
       $('html,body').animate({
-
-        scrollTop: $('.about-section').offset().top},
+        scrollTop: $('.about').offset().top},
         'slow');
       return false;
       });        
--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,4 +1,4 @@
-/*global $*/
+// global $ //
 document.addEventListener("turbolinks:load", function() {
   var menuToggle = $("#js-mobile-menu").off();
   $("#js-navigation-menu").removeClass("show");
@@ -6,11 +6,11 @@ document.addEventListener("turbolinks:load", function() {
     e.preventDefault();
     $('.burger-toggle').toggleClass('opened'); 
     if($('.navigation-wrapper').hasClass('clear')) {
-      $('.hero').css({"opacity":"0.7", "transition":"opacity 0.4s ease"});
-      $('.navigation-wrapper').css({"background-color":"darkslategrey", 
-                            "transition":"background-color 0.1s ease"});
       $('.navigation-wrapper').removeClass('clear');
       $('.navigation-wrapper').addClass('active');
+      $('.navigation-wrapper').css({"background-color":"darkslategrey", 
+                            "transition":"background-color 0.1s ease"});
+      $('.hero').css({"opacity":"0.7", "transition":"opacity 0.4s ease"});
     } else if($('.navigation-wrapper').hasClass('active')) {
       $('.hero').css({"opacity":"1.0"});
       $('.navigation-wrapper').css({"background-color":"rgba(0,0,0,0.06)", 
@@ -20,7 +20,6 @@ document.addEventListener("turbolinks:load", function() {
     } else if($('.navigation-wrapper').hasClass('dark')) {
       $('.navigation-wrapper').toggleClass('dark')
     }
-    
     $("#js-navigation-menu").slideToggle(function(){
       if ($("#js-navigation-menu").is(":hidden")) {
         $("#js-navigation-menu").removeAttr("style");
@@ -28,3 +27,4 @@ document.addEventListener("turbolinks:load", function() {
     });
   }); 
 });
+
